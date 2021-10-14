4b33b58d72710e84a07d991ec1f2df09a5d2ed42
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-14 18:44:13 +0530

    lec 31: examples of why we care about types in array

--- features/arrays.ts
+++ features/arrays.ts
@@ -20,3 +20,39 @@ const carsByMake: string[][] = [
     ['corolla'],
     ['camaro']
 ]
+
+/*
+
+lec 30
+
+Why do we care about types in array ?
+
+1. TS can do type inference when extracting values from an array.
+2. TS can prevent us from adding incompatible values to the array.
+3. We can get help with 'map', 'forEach', 'reduce', functions
+4. Flexible - arrays can still contain multiple different types
+
+*/
+
+
+/*
+
+lec 31
+
+*/
+// 1. Help with inference when extracting values ////////////////
+const car = carMakers[0]; // hover over car and ts knows its a string
+const myCar = carMakers.pop(); // myCar is string 
+
+// 2. Prevent incompatible values //////////////////
+// carMakers.push(100);
+// Argument of type 'number' is not assignable to parameter of type 'string'.
+
+// 3. Help with 'map'////////////////////
+carMakers.map((car: string) => {
+    return car.toUpperCase();
+})
+
+// 4. Flexible types
+const importantDates = [new Date(), '2030-10-10']; // inferred (string | Date)[]
+const importantDatesX: (Date | string)[] = [new Date(), '2030-10-10']; 
