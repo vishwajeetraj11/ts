e1b577214298025ff3f818604d09262e8548a24a
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-14 17:24:57 +0530

    lec 26 functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -13,3 +13,31 @@ Type inference doesn't works on arguments of the  function but it does works on 
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a:number, b:number): number {
+  return a/b;
+}
+
+const multiply = function(a:number, b:number): number {
+  return a*b;
+}
+
+// lec 26 
+const logger = (message:string): void => {
+  console.log(message)
+}
+
+const throwError = (message: string): never => {
+  // never => we are never going to execute the function completely at some point in time we are going to throw error and exit the function early without returning any value
+  throw new Error(message);
+}
+
+const throwErrorAlt = (message: string): string => {
+  // never => we are never going to execute the function completely at some point in time we are going to throw error and exit the function early without returning any value
+  if(!message) throw new Error('No message found.');
+  else return message
+}
