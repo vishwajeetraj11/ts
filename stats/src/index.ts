2d7fbe66d1c2e7f624c571dbf3be7e226fd2b2f0
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-21 23:01:43 +0530

    [stats][101]: Detailed Introduction to Enums

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -63,4 +63,9 @@ Current Issues:
 lec 100:-
 enum - enumeration
 
+Enums
+- Follow near-identical syntax rules as normal objects.
+- Creates an object with the same keys and values when converted from TS to JS.
+- Primary goal is to signal to other engineers that these are all closely related values.
+- Use whenever we have a small fixed set of values that are all closely related are known at compile time.
 */
