e2b357ea4abe1991b7e1e54fe4c13c00cc7a35a9
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-10-21 21:03:02 +0530

    [stats][96]: Added Node.js Types

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hi')
+/*
+lec 96
+{console.log('If you are reading this you are a genius.')}
+Node.js docs file system module. (EXPLORING)
+*/
+
+/* Cannot find module fs. (because of no types present)
+npm i @types/node
+*/
+import fs from 'fs';
