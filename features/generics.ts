bd484444d767ef1fccffd3749f20779356ef91a7
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-11-28 14:52:21 +0530

    [generics][131-132]: Examples of generics with functions

--- features/generics.ts
+++ features/generics.ts
@@ -22,3 +22,23 @@ class ArrayOfAnything<T> {
 }
 
 new ArrayOfAnything<string>(['a', 'b', 'c'])
+
+/* Examples of generics with functions */
+
+function printStrings(arr: string[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
+}
+
+function printNumbers(arr: number[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
+}
+
+function printAnything<T>(arr: T[]): void {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
+}
