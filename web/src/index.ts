e9762566ecee4fd120b6142b02f3bbaea57e9e9e
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-11-28 16:13:20 +0530

    [web][139]: Added setters for user class

--- web/src/index.ts
+++ web/src/index.ts
@@ -21,5 +21,7 @@ import { User } from "./models/User";
 
 const user = new User({ name: 'Vishwajeet Raj', age: 20 });
 
+user.set({ name: 'Aples', age: 999 });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
