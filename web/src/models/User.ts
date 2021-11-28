5fcc6b1e0e5f0ba598715ec67bd750ad89a03f93
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-11-28 21:48:25 +0530

    [web][140-142]: Added Events Handling Type

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -3,7 +3,19 @@ interface UserProps {
     age?: number
 }
 
+type Callback = () => void
+
 export class User {
+
+    /*
+    Usually when we write a type annotation for an object, we just write out the name of the property 
+    Ex- click ==> events: { [key: string]: Callback[] } = {};
+    But in this case we don't know thw names of the events that we are going to have,
+    We might have eventually an event tied to a user called click or hover or mouseover or maybe like change or sync or delete. 
+    The point is we just don't know it yet what the different events name are that we will have tied to a user so to solve that we say key: string.
+    */
+    events: { [key: string]: Callback[] } = {};
+
     constructor(private data: UserProps) { }
 
     get(propName: string): (string | number) {
@@ -12,5 +24,9 @@ export class User {
 
     set(update: UserProps): void {
         Object.assign(this.data, update);
+    }
+
+    on(eventName: string, callback: Callback): void {
+        console.log(eventName);
     }
 }
