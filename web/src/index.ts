9459b7b5f7a375f2bfebbbea05f9782494f4cea2
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-12-28 21:35:11 +0530

    [web][188]: Show User Data in Form

--- web/src/index.ts
+++ web/src/index.ts
@@ -23,10 +23,13 @@ npm i -g json-server
 json-server -w db.json
 */
 
+import { User } from './models/User';
 import { UserForm } from './views/UserForm';
 
+const user = User.buildUser({ name: 'NAME', age: 40 })
+
 const userForm = new UserForm(
-    document.getElementById('root')
+    document.getElementById('root'), user
 );
 
 userForm.render()
--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,9 +1,13 @@
+import { User } from "../models/User";
+
 export class UserForm {
-    constructor(public parent: Element) { }
+    constructor(public parent: Element, public model: User) { }
     template(): string {
         return `
             <div>
             <h1>User Form</h1>
+            <div>User name: ${this.model.get('name')}</div>
+            <div>User age: ${this.model.get('age')}</div>
             <input />
             <button>Click Me</button>
             </div>
