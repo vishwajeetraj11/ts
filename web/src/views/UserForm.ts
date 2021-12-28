f118ae0a81dede131b042004006a69ab89ecc999
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-12-28 21:59:07 +0530

    [web][192]: Implement Change Name feature

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -18,7 +18,7 @@ export class UserForm {
             <div>User name: ${this.model.get('name')}</div>
             <div>User age: ${this.model.get('age')}</div>
             <input />
-            <button>Click Me</button>
+            <button class='set-name'>Change Name</button>
             <button class='set-age'>Set Random Age</button>
             </div>
         `
@@ -28,9 +28,16 @@ export class UserForm {
         this.model.setRandomAge()
     }
 
+    onSetNameClick = (): void => {
+        const input = this.parent.querySelector('input');
+        const name = input.value;
+        this.model.set({ name })
+    }
+
     eventsMap(): { [key: string]: () => void } {
         return {
             'click:.set-age': this.onSetAgeClick,
+            'click:.set-name': this.onSetNameClick
         };
     }
 
