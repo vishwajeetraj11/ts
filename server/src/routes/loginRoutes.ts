2615a0ff74ec475340604fae9330d1f28803845a
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2022-01-02 23:03:43 +0530

    [server][214]: Defining an Interface for req body property

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Request, Response, Router } from 'express';
 
+interface RequestWithBody extends Request {
+    body: { [key: string]: string | undefined }
+}
+
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
@@ -18,19 +22,23 @@ router.get('/login', (req: Request, res: Response) => {
     `)
 })
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
     const { email, password } = req.body;
 
-    if (!email) {
+    if (email) {
+        if (email === 'hi@hi.com' && password === 'password') {
+            req.session = { loggedIn: true };
+            res.redirect('/');
+        } else {
+            res.send('Invalid email or password');
+        }
+    }
+    else {
         res.send('Please send email.')
     }
 
-    if (email === 'hi@hi.com' && password === 'password') {
-        req.session = { loggedIn: true };
-        res.redirect('/');
-    } else {
-        res.send('Invalid email or password');
-    }
+
+
 })
 
 export { router };
