e3c6831595a7cbeda7d496949e01182154896be7
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2022-01-02 23:54:32 +0530

    [server][218]: Implementing Protected Route

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,17 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 
 interface RequestWithBody extends Request {
     body: { [key: string]: string | undefined }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+
+    res.status(403);
+    res.send('Not permitted');
 }
 
 const router = Router();
@@ -58,6 +68,10 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
 router.get('/logout', (req: Request, res: Response) => {
     req.session = undefined;
     res.redirect('/');
+})
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+    res.send('Welcome to protected route, logged in user');
 })
 
 export { router };
