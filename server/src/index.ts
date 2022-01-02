85dca1859c464e181fa16a93e44085883d042915
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2022-01-02 22:53:44 +0530

    [server][211-213]: Express Routes, Poor type defs express TS.

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,17 +8,25 @@ TS with JS Libraries.
 - Use the library normally, adding in basic type annotations where possible.
 - Use a TS adapter library that has helpers for using your library with TS.
 - Twist your lib to work with TS classes.
+
+Integration Issues:
+
+CONS-
+    1. Type definition files alone can't express what is going on in the JS world accurately (example: middleware).
+    2. Type definition files provided to us aren't always accurate. ex- req.body has type any, which is incorrect unless a body parser has been used.
+    3. Inputs to a server (or any program with external inputs) are not guranteed to exist, or to be of the correct type. (the type definition file makes it even worse)
+PROS-
+    1. Addressing these type of issues with Typescript can force us to write better code.
 */
 
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes'
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(
-        '<div>hi</div>'
-    )
-})
+app.use(router)
 
 app.listen(3000, () => {
     console.log('Listening on port: 3000')
---
+++ server/src/routes/loginRoutes.ts
@@ -0,0 +1,36 @@
+import { Request, Response, Router } from 'express';
+
+const router = Router();
+
+router.get('/login', (req: Request, res: Response) => {
+    res.send(`
+    <form method="POST">
+        <div>
+          <label>Email</label>
+          <input name="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+      </form>
+    `)
+})
+
+router.post('/login', (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        res.send('Please send email.')
+    }
+
+    if (email === 'hi@hi.com' && password === 'password') {
+        req.session = { loggedIn: true };
+        res.redirect('/');
+    } else {
+        res.send('Invalid email or password');
+    }
+})
+
+export { router };
