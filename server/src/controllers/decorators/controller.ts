6386c19327f2d14e4dd6f72dcf0ec09ed35d85fa
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2022-01-18 20:18:22 +0530

    [server][241]: Methods refactor

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 
 import { AppRouter } from '../../appRouter';
-
+import { Methods } from './methods';
 
 export function controller(routePrefix: string) {
     return function (target: Function) {
@@ -9,9 +9,10 @@ export function controller(routePrefix: string) {
         for (let key in target.prototype) {
             const routeHandler = target.prototype[key];
             const path = Reflect.getMetadata('path', target.prototype, key);
+            const method: Methods = Reflect.getMetadata('method', target.prototype, key);
 
             if (path) {
-                router.get(`${routePrefix}${path}`, routeHandler);
+                router[method](`${routePrefix}${path}`, routeHandler);
             }
         }
     }
