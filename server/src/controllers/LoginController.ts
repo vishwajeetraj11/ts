92f499a484ade5254fb02b98d66710485341a8a6
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2022-01-18 21:26:24 +0530

    [server][244]: Using @use for middlewares.

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -1,11 +1,17 @@
-import { Request, Response } from "express";
-import { controller, get } from "./decorators"
+import { NextFunction, Request, Response } from "express";
+import { controller, get, use } from "./decorators"
+
+function logger(req: Request, res: Response, next: NextFunction) {
+  console.log('Request was made!!!');
+  next();
+}
 
 @controller('/auth')
 class LoginController {
-    @get('/login')
-    getLogin(req: Request, res: Response): void {
-        res.send(`
+  @get('/login')
+  @use(logger)
+  getLogin(req: Request, res: Response): void {
+    res.send(`
         <form method="POST">
             <div>
               <label>Email</label>
@@ -18,5 +24,5 @@ class LoginController {
             <button>Submit</button>
           </form>
         `)
-    }
+  }
 }
