92fa2498ccdc84f29f9488de2f9abc64ded95303
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2022-01-18 16:44:24 +0530

    [server][225-228]: Decorators Examples.

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,26 +1,68 @@
+@classDecorator
 class Boat {
+    @testDecorator
     color: string = 'red';
 
+    @testDecorator
     get formattedColor(): string {
         return `This boats color is ${this.color}`;
     }
 
-    @logError
-    pilot(): void {
-        throw new Error();
-        console.log('swish');
+    // @logError('Oops boat was sunk in ocean')
+    @logError('Something bad!')
+    pilot(
+        @parameterDecorator speed: string,
+        @parameterDecorator generateWake: boolean
+    ): void {
+        if (speed === 'fast') {
+            console.log('swish');
+        } else {
+            console.log('nothing');
+        }
     }
 }
 
+function classDecorator(constructor: typeof Boat) {
+    console.log(constructor); 
+}
 
-// function testDecorator(target: any, key: string) {
-//     console.log("Target ", target);
-//     console.log("Key ", key);
-// }
+function parameterDecorator(target: any, key: string, index: number) {
+    console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+    // console.log("Target ", target);
+    console.log("Key ", key);
+    // console.log(target.color);
+    // console.log(target[key]);
+}
 
 // testDecorator(Boat.prototype, 'pilot')
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-    console.log(`Target: ${target}`)
-    console.log(`Key: ${key}`)
+// function logError(target: any, key: string, desc: PropertyDescriptor): void {
+//     const method = desc.value;
+//     desc.value = function () {
+//         try {
+//             method();
+//         } catch (e) {
+//             console.log('Oops, boat was sunk');
+//         }
+//     }
+// }
+
+// Decorator Factory = A function that returns a decorator.
+
+function logError(errorMessage: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor): void {
+        const method = desc.value;
+        desc.value = function () {
+            try {
+                method();
+            } catch (e) {
+                console.log(errorMessage);
+            }
+        }
+    }
 }
+
+// new Boat().pilot();
